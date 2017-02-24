1ad8b6ca98f85a90a539ae72c4236f118707e34a
Ravi <rkappagantu@gmail.com>
2017-02-24 10:31:20 +0530

    Lambda Sample function

---
+++ src/lambdasample/LambdaFunctionHandler.java
@@ -0,0 +1,19 @@
+package lambdasample;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import com.amazonaws.services.lambda.runtime.events.S3Event;
+
+public class LambdaFunctionHandler implements RequestHandler<S3Event, Object> {
+
+    @Override
+    public Object handleRequest(S3Event input, Context context) {
+        context.getLogger().log("Input: " + input);
+        context.getLogger().log("Hello World from AWS Lambda");
+
+        // TODO: implement your handler
+        return null;
+    }
+
+}

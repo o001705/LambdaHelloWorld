d69acbf7de3ed4028984a1607c295c636e881313
Ravi <rkappagantu@gmail.com>
2017-02-24 11:20:34 +0530

    Added S3output

--- src/lambdasample/LambdaFunctionHandler.java
+++ src/lambdasample/LambdaFunctionHandler.java
@@ -1,16 +1,38 @@
 package lambdasample;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.event.S3EventNotification.S3EventNotificationRecord;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
 
 public class LambdaFunctionHandler implements RequestHandler<S3Event, Object> {
 
     @Override
     public Object handleRequest(S3Event input, Context context) {
-        context.getLogger().log("Input: " + input);
-        context.getLogger().log("Hello World from AWS Lambda");
+        S3Sample obj = new S3Sample();
+        AmazonS3Client s3Client = new AmazonS3Client
+        		(new DefaultAWSCredentialsProviderChain());        
+
+        for (S3EventNotificationRecord record : input.getRecords()) {
+            String s3Key = record.getS3().getObject().getKey();
+            String s3Bucket = record.getS3().getBucket().getName();
+            context.getLogger().log("found id: " + s3Bucket+" "+s3Key);
+            // retrieve s3 object
+            S3Object object = s3Client.getObject(new GetObjectRequest(s3Bucket, s3Key));
+            try{
+            obj.WritetoS3Bucket("raviksampleoutput", object);
+            } catch (IOException e) {}
+        }               
+        
+        
 
         // TODO: implement your handler
         return null;
---
+++ src/lambdasample/S3Sample.java
@@ -0,0 +1,61 @@
+package lambdasample;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.UUID;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+
+public class S3Sample {
+
+    public void WritetoS3Bucket(String bucketName, S3Object item) throws IOException {
+
+        /*
+         * The ProfileCredentialsProvider will return your [default]
+         * credential profile by reading from the credentials file located at
+         * (C:\\Users\\rlsg\\.aws\\credentials).
+         */
+        AWSCredentials credentials = null;
+        try {
+            credentials = new ProfileCredentialsProvider("default").getCredentials();
+        } catch (Exception e) {
+            throw new AmazonClientException(
+                    "Cannot load the credentials from the credential profiles file. " +
+                    "Please make sure that your credentials file is at the correct " +
+                    "location (C:\\Users\\rlsg\\.aws\\credentials), and is in valid format.",
+                    e);
+        }
+
+        AmazonS3 s3 = new AmazonS3Client(credentials);
+        Region usEast1 = Region.getRegion(Regions.US_EAST_1);
+        s3.setRegion(usEast1);
+
+        String key = item.getKey();
+
+        try {
+            s3.putObject(new PutObjectRequest(bucketName, key, item.getObjectContent(), item.getObjectMetadata()));
+        } catch (AmazonServiceException ase) {
+        } catch (AmazonClientException ace) {
+        }
+    }
+}
